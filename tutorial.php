bc8fcf8d5653cba0ef9ead3f16b55ff9123cace2
romaimperator <romaimperator@gmail.com>
2012-03-24 05:12:19 -0500

    Added the tutorial and images.

--- tutorial.php
+++ tutorial.php
@@ -2,4 +2,18 @@
   <div class="page-header">
     <h1>Tutorial</h1>
   </div>
+  <div class="row">
+    <ol>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">Download and install the addon.</span><img class="thumbnail" src="img/screen-1.png"/></li>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">Click to allow the addon installation.</span><img class="thumbnail" src="img/screen-2.png"/></li>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">Click install now to complete the addon installation.</span><img class="thumbnail" src="img/screen-3.png"/></li>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">If you see this the addon was successfully installed.</span><img class="thumbnail" src="img/screen-4.png"/></li>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">Find the Foamicator button (it looks like a beer glass) and click on it. Then click on login.</span><img class="thumbnail" src="img/screen-5.png"/></li>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">Since you just installed the addon, Foamicator will ask you to create a master password. Remember this because it is required to unlock the addon after restarting Firefox.</span><img class="thumbnail" src="img/screen-6.png"/></li>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">Now that you've created a password you can login to this demo site by clicking login again.</span><img class="thumbnail" src="img/screen-5.png"/></li>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">After a few moments, Foamicator will have finished generating your key and will log you into the site.</span><img class="thumbnail" src="img/screen-7.png"/></li>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">As a test, try leaving a note for yourself.</span><img class="thumbnail" src="img/screen-8.png"/></li>
+      <li class="span12 foam_pad_40"><span class="foam_pad_20">After you click logout, click login again and your note should be there!</span><img class="thumbnail" src="img/screen-9.png"/></li>
+    </ol>
+  </div>
 </div>
