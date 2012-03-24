fa7351bbd7b5c02638377a891ee1b87481814d12
romaimperator <romaimperator@gmail.com>
2012-03-24 18:38:17 -0500

    Changed the CSS selectors to be a bit more efficient.

--- tutorial.php
+++ tutorial.php
@@ -2,18 +2,18 @@
   <div class="page-header">
     <h1>Tutorial</h1>
   </div>
-  <div class="row foamicator-tutorial">
+  <div class="row">
     <ol>
-      <li class="span12">Download and install the addon.<img class="thumbnail" src="img/screen-1.png"/></li>
-      <li class="span12">Click to allow the addon installation.<img class="thumbnail" src="img/screen-2.png"/></li>
-      <li class="span12">Click install now to complete the addon installation.<img class="thumbnail" src="img/screen-3.png"/></li>
-      <li class="span12">If you see this the addon was successfully installed.<img class="thumbnail" src="img/screen-4.png"/></li>
-      <li class="span12">Find the Foamicator button (it looks like a beer glass) and click on it. Then click on login.<img class="thumbnail" src="img/screen-5.png"/></li>
-      <li class="span12">Since you just installed the addon, Foamicator will ask you to create a master password. Remember this because it is required to unlock the addon after restarting Firefox.<img class="thumbnail" src="img/screen-6.png"/></li>
-      <li class="span12">Now that you've created a password you can login to this demo site by clicking login again.<img class="thumbnail" src="img/screen-5.png"/></li>
-      <li class="span12">After a few moments, Foamicator will have finished generating your key and will log you into the site.<img class="thumbnail" src="img/screen-7.png"/></li>
-      <li class="span12">As a test, try leaving a note for yourself.<img class="thumbnail" src="img/screen-8.png"/></li>
-      <li class="span12">After you click logout, click login again and your note should be there!<img class="thumbnail" src="img/screen-9.png"/></li>
+      <li class="foamicator-tutorial-li span12">Download and install the addon.<img class="foamicator-tutorial-img thumbnail" src="img/screen-1.png"/></li>
+      <li class="foamicator-tutorial-li span12">Click to allow the addon installation.<img class="foamicator-tutorial-img thumbnail" src="img/screen-2.png"/></li>
+      <li class="foamicator-tutorial-li span12">Click install now to complete the addon installation.<img class="foamicator-tutorial-img thumbnail" src="img/screen-3.png"/></li>
+      <li class="foamicator-tutorial-li span12">If you see this the addon was successfully installed.<img class="foamicator-tutorial-img thumbnail" src="img/screen-4.png"/></li>
+      <li class="foamicator-tutorial-li span12">Find the Foamicator button (it looks like a beer glass) and click on it. Then click on login.<img class="foamicator-tutorial-img thumbnail" src="img/screen-5.png"/></li>
+      <li class="foamicator-tutorial-li span12">Since you just installed the addon, Foamicator will ask you to create a master password. Remember this because it is required to unlock the addon after restarting Firefox.<img class="foamicator-tutorial-img thumbnail" src="img/screen-6.png"/></li>
+      <li class="foamicator-tutorial-li span12">Now that you've created a password you can login to this demo site by clicking login again.<img class="foamicator-tutorial-img thumbnail" src="img/screen-5.png"/></li>
+      <li class="foamicator-tutorial-li span12">After a few moments, Foamicator will have finished generating your key and will log you into the site.<img class="foamicator-tutorial-img thumbnail" src="img/screen-7.png"/></li>
+      <li class="foamicator-tutorial-li span12">As a test, try leaving a note for yourself.<img class="foamicator-tutorial-img thumbnail" src="img/screen-8.png"/></li>
+      <li class="foamicator-tutorial-li span12">After you click logout, click login again and your note should be there!<img class="foamicator-tutorial-img thumbnail" src="img/screen-9.png"/></li>
     </ol>
   </div>
 </div>
